998a48a31a2805fa926086a363390de19843a084
Logan Joecks <ljoecks@gmail.com>
2014-08-01 14:09:45 -0400

    tweak landing page

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
 
         //Change this to your own rss feeds
-        "http://loganjoecks.com/blog/rss/",
+        "http://logstown.github.io/blog/rss/",
 
         {
             // how many entries do you want?
